b20261c467f7eecf0c8ba47d30c8b0e77f75e287
Francis <francisdinh@outlook.com>
2017-06-14 00:24:32 -0700

    Fix bug for Mac OS

--- src/main/java/com/bird/puffin/Window.java
+++ src/main/java/com/bird/puffin/Window.java
@@ -1,6 +1,7 @@
 package com.bird.puffin;
 
 import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
 
 public class Window {
 	private long window;
@@ -8,6 +9,7 @@ public class Window {
 	public Window(int width, int height) {
 		glfwInit();
 		window = glfwCreateWindow(width, height, "Puffin", 0, 0);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	}
 	
 	public long getWindow() {
