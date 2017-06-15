7022a69b609350d0c181bd90426c5ea8b985f41f
Francis <ftdinh@ucsd.edu>
2017-06-15 12:33:12 -0700

    Fixed bug regarding opengl core profile 3.3 resulting in no display

--- src/main/java/com/bird/puffin/Window.java
+++ src/main/java/com/bird/puffin/Window.java
@@ -8,8 +8,11 @@ public class Window {
 
 	public Window(int width, int height) {
 		glfwInit();
-		window = glfwCreateWindow(width, height, "Puffin", 0, 0);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		window = glfwCreateWindow(width, height, "Puffin", 0, 0);
 	}
 	
 	public long getWindow() {
